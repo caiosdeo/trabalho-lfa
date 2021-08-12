1fd7dcce6d2b03ac44ab07a0e5af5c62a67c799f
caiosdeo <caiosdeo@hotmail.com>
2021-08-12 12:42:05 -0300

    Adicionando classe Simbolo

--- src/classes/Estado.py
+++ src/classes/Estado.py
@@ -1,21 +1,49 @@
 class Estado:
-    nome = ""
 
     def __init__(self, nome=""):
+        """Construtor do Estado 
+
+        Args:
+            nome (str): String a ser definida como nome do Estado 
+        """
         self.nome = nome
 
     def getNome(self):
+        """Obtem o nome do Estado
+
+        Returns:
+            nome (str): String que representa o nome do Estado
+        """
         return self.nome
 
     def setNome(self, nome):
+        """Ajusta o nome do Estado para o valor passado como parametro
+
+        Args:
+            nome (str): String a ser definida como nome do Estado 
+        """
         self.nome = nome
 
     def clonar(self):
+        """Cria e retorna uma copia do objeto Estado
+
+        Returns:
+            e (Estado): clone do Estado
+        """
         e = Estado()
         e.setNome(self.getNome())
         return e
 
     def igual(self, estado):
+        """Verifica se o Estado passado por parametro é igual ao proprio Estado
+        
+        Args:
+            estado (Estado): Estado a ser comparado 
+
+        Returns:
+            igual (bool): flag para igualdade dos Estados
+                (default é False)
+        """
         if (self.nome == estado.getNome()):
             return True
         return False
---
+++ src/classes/Simbolo.py
@@ -0,0 +1,53 @@
+class Simbolo:
+    VAZIA = Simbolo("E")
+
+    def __init__(self, simbolo=""):
+        """Construtor do Simbolo 
+
+        Args:
+            simbolo (str): String a ser definida como simbolo do alfabeto 
+        """
+        self.simbolo = simbolo
+
+    def getSimbolo(self):
+        """Obtem o simbolo do alfabeto
+
+        Returns:
+            simbolo (str): String que representa o simbolo do alfabeto
+        """
+        return self.simbolo
+
+    def setSimbolo(self, simbolo):
+        """Ajusta o simbolo do alfabeto para o valor passado como parametro
+
+        Args:
+            simbolo (str): String a ser definida como simbolo do alfabeto    
+        """
+        self.simbolo = simbolo
+
+    def clonar(self):
+        """Cria e retorna uma copia do objeto Simbolo
+
+        Returns:
+            s (Simbolo): clone do Simbolo
+        """
+        s = Simbolo()
+        s.setSimbolo(self.getSimbolo())
+        return s
+
+    def igual(self, simbolo):
+        """Verifica se o Simbolo passado por parametro é igual ao proprio Simbolo
+        
+        Args:
+            simbolo (Simbolo): Simbolo a ser comparado
+
+        Returns:
+            igual (bool): flag para igualdade dos Simbolos
+                (default é False)
+        """
+        if (self.simbolo == simbolo.getSimbolo()):
+            return True
+        return False
+    
+    def __str__(self):
+        return self.simbolo
